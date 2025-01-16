cc7ffa07944df89dd98cfd0983eecad3ea71907c
LCSOGthb <185141600+LCSOGthb@users.noreply.github.com>
2025-01-16 08:28:39 +0800

    Create (#30)

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("uselessButton").addEventListener("click", function() {
+    const text = document.getElementById("uselessText");
+    text.textContent = "You clicked the button... for no reason!";
+});
